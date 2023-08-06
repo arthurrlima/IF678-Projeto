6b72eef7703f8c8a2ab390ffad290f53101efbfe
Arthur Lima <arl3@cin.ufpe.br>
2023-08-06 20:48:07 -0300

    Adicionado arquivo .jpg

--- UDPClient.py
+++ UDPClient.py
@@ -1,13 +1,15 @@
 import socket
 import time
 import sys
+import select
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 buf = 1024
-file_name = "demofile.txt"
+file_name = "demofile32.jpg"
+timeout = 3
 
-f = open(file_name, "r")
+f = open(file_name, "rb")
 data = f.read(buf)
 
 UDPClient_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -18,19 +20,25 @@ print ("Enviando %s ..." % file_name)
 
 
 while(data):
-    if(UDPClient_sock.sendto(data.encode(), (UDP_IP, UDP_PORT))):
+    if(UDPClient_sock.sendto(data, (UDP_IP, UDP_PORT))):
         data = f.read(buf)
         time.sleep(0.02) # Delay antes de fechar o arquivo
 f.close()
 
 #Recebendo Arquivo de volta.
-receivedBytes, adress  = UDPClient_sock.recvfrom(buf)
+f2 = open("cl_files/"+file_name, 'wb')
+while True:
+    ready = select.select([UDPClient_sock], [], [], timeout)
+    if ready[0]:
+        receivedBytes, adress  = UDPClient_sock.recvfrom(buf)
+        f2.write(receivedBytes)
+    else:
+        print("Arquivo Recebido!")
+        f2.close()
+        break
+    
 
 #Salvando Arquivo recebido.
-f2 = open("cl_files/"+file_name, 'x')
-f2.write(receivedBytes.decode('ascii'))
-f2.close
-print ("Arquivo Recebido.")
-
+#Fechando Socket
 UDPClient_sock.close()
 print ("Conexão Encerrada.")
--- UDPServer.py
+++ UDPServer.py
@@ -1,5 +1,7 @@
 import socket
 import select
+import time
+import sys
 
 UDP_IP = "127.0.0.1"
 IN_PORT = 5005
@@ -16,19 +18,30 @@ while True:
         print ("Conexão Estabelecida! \n Aguardando Arquivo...")
         print ("Recebendo arquivo: ")
         print ("File name:", data.decode('ascii'))
-        file_name = data.strip()
+        file_name = data.strip().decode('ascii')
 
-    f = open("sv_files/demofile.txt", 'x')
+    f = open("sv_files/"+file_name, 'wb')
 
     while True:
         ready = select.select([Server_sock], [], [], timeout)
         if ready[0]:
             data, addr = Server_sock.recvfrom(1024)
-            f.write(data.decode('ascii'))
+            f.write(data)
         else:
             print ("%s Finish!" % file_name)
             f.close()
+            
 
             #Devolvendo Arquivo
-            Server_sock.sendto(data, addr)
+            print ("Devolvendo Arquivo ao Cliente...")
+            f2 = open("sv_files/"+file_name, 'rb')
+            data2 = f2.read(1024)
+
+            while(data2):
+                if(Server_sock.sendto(data2, addr)):
+                    data2 = f2.read(1024)
+                    time.sleep(0.02)
+            f2.close()
+            print ("Arquivo Devolvido!")
             break
+        
