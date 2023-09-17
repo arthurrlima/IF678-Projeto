53cc3fad745030c797672fae5ea33227119dd4b2
Arthur Scanoni <61999297+Arthursca@users.noreply.github.com>
2023-09-17 13:15:40 -0300

    refactor: organize functions

--- Client/Client.py
+++ Client/Client.py
@@ -1,56 +1,78 @@
 import socket
 
-def send_file(filename, client_socket):
+def make_pkt(file):
+    return file.read(1024)
+
+def udp_send(packet, data, client_socket):
+    client_socket.sendto(data, packet)
+
+def extract(client_socket):
+    data, addr = client_socket.recvfrom(1024)
+    return data, addr
+
+def deliver_data(file, data):
+    return file.write(data)
+
+
+
+
+
+def send_file(filename, client_socket, packet):
     with open(filename, 'rb') as f:
-        data = f.read(1024)
+        data = make_pkt(f)
+
         while data:
-            client_socket.sendto(data, (HOST, PORT))
-            last_data = data
-            data = f.read(1024)
+
+            udp_send(packet, data, client_socket)
+            data = make_pkt(f)
+
             if not data:
-                client_socket.sendto(b"<end>", (HOST, PORT))
+                udp_send(packet, b"<end>", client_socket)
     print(f"Arquivo {filename} enviado com sucesso!")
 
+#rtd_rcv(packet)
 def recive_file(filename, client_socket):
+
+
     with open(filename, 'wb') as f:
             while True:
-                data, addr = client_socket.recvfrom(1024)
+                # extract(packet, data)
+                data, addr = extract(client_socket)
                 if data == b"File Not Found":
                     print("Arquivo não encontrado no servidor.")
                     break
                 if data == b"<end>":
                     print(f"Arquivo {filename} recebido com sucesso.")
                     break
-                if data != b'':
-                    f.write(data)
+
+                elif data != b'':
+                    deliver_data(f, data)
 
 # Configurações do cliente
 HOST = 'localhost'
 PORT = 12345
 
+
+packet = (HOST, PORT)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
     action = input("Digite 'enviar' para enviar um arquivo ou 'receber' para receber um arquivo (ou 'exit' para sair): ").lower()
 
     if action == 'exit':
-        client_socket.sendto(action.encode(), (HOST, PORT))
+        client_socket.sendto(action.encode(), packet)
         print("Cliente encerrado.")
         break
 
     if action == 'enviar':
         filename = input("Digite o nome do arquivo a ser enviado: ")
-        client_socket.sendto(f"{action} {filename}".encode(), (HOST, PORT))
-        send_file(filename, client_socket)
+        client_socket.sendto(f"{action} {filename}".encode(), packet)
+        send_file(filename, client_socket, packet)
 
     elif action == 'receber':
         filename = input("Digite o nome do arquivo a ser recebido: ")
-        client_socket.sendto(f"{action} {filename}".encode(), (HOST, PORT))
+        client_socket.sendto(f"{action} {filename}".encode(), packet)
         recive_file(filename, client_socket)
-        
-
-
-
 
     else:
         print("Opção inválida. Tente novamente.")
--- Server/Server.py
+++ Server/Server.py
@@ -1,36 +1,61 @@
 import socket
 
-def receive_file(filename, client_address, server_socket):
-    with open(filename, 'wb') as f:
-        while True:
-            data, addr = server_socket.recvfrom(1024)
-            if data == b"<end>":
-                break
-            if addr == client_address and data != b'':
-                f.write(data)
 
-    print(f"Arquivo {filename} recebido com sucesso!")
+def make_pkt(file):
+    return file.read(1024)
 
+
+def extract(server_socket):
+    data, addr = server_socket.recvfrom(1024)
+    return data, addr
+
+
+def udp_send(server_socket, data, client_address):
+    server_socket.sendto(data, client_address)
+
+
+def deliver_data(file, data):
+    return file.write(data)
+
+
+# rtd_send(data)
 def send_file(filename, client_address, server_socket):
     try:
         with open(filename, 'rb') as f:
-            data = f.read(1024)
+            data = make_pkt(f)
+
             while data:
-                server_socket.sendto(data, client_address)
-                last_data = data
-                data = f.read(1024)
+
+                # udp_send(packet, data)
+                udp_send(server_socket, data, client_address)
+                data = make_pkt(f)
+
                 if not data:
-                    server_socket.sendto(b"<end>", client_address)
-
+                    udp_send(server_socket, b"<end>", client_address)
         print(f"Arquivo {filename} enviado para o cliente.")
+
     except FileNotFoundError:
-        server_socket.sendto(b"File Not Found", client_address)
+        print("Arquivo não encontrado no servidor.")
+        udp_send(server_socket, b"File Not Found", client_address)
 
 
-# Configurações do servidor
+# rtd_rcv(packet)
+def receive_file(filename, client_address, server_socket):
+    with open(filename, 'wb') as f:
+        while True:
+            data, addr = extract(server_socket)
+
+            if data == b"<end>":
+                break
+
+            if addr == client_address and data != b'':
+                deliver_data(f, data)
+
+    print(f"Arquivo {filename} recebido com sucesso!")
+
+
 HOST = 'localhost'
 PORT = 12345
-
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((HOST, PORT))
 
@@ -47,7 +72,8 @@ while True:
     action, filename = data.split()
     if action.lower() == 'enviar':
         receive_file(filename, client_address, server_socket)
+
     elif action.lower() == 'receber':
-        send_file(filename, client_address, server_socket)        
+        send_file(filename, client_address, server_socket)
 
 server_socket.close()
